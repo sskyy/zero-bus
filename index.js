c5a3a889eb61c529af11597086adfb0b30adc7fe
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-11-16 20:34:14 +0800

    allow one module attach multiple listener on one event, and auto release 0.0.10

--- index.js
+++ index.js
@@ -23,22 +23,26 @@ module.exports = {
   expand : function( module ){
     var root = this
     if( module.listen ){
-      _.forEach(module.listen, function( listener, event){
-        root.on( event, listener, module.name)
+      _.forEach(module.listen, function( listeners, event){
+        //make it a array
+        listeners = [].concat(listeners)
+        listeners.forEach(function(listener){
+          root.on( event, listener, module.name)
+        })
       })
     }
   },
   on : function( event, listener, moduleName ){
     var root = this
-    root.bus.module(moduleName || root.relier)
+    moduleName = moduleName || root.relier
+    root.bus.module(moduleName )
     root.bus.on( event, listener)
 
     if( !root.listeners[event] ) root.listeners[event] = []
 
-    root.listeners[event].push( {
-      module : moduleName || root.relier,
-      listener : listener.name || "anonymous"
-    })
-
+    root.listeners[event].push(
+      _.defaults({
+        module : moduleName || root.relier
+      },_.isFunction(listener)?{ "function":listener,"name":listener.name||"anonymous"}: listener  ))
   }
 }
